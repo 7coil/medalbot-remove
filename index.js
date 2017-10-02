74776655d9c15b0224de82a242b9426f34630770
lepon01 <leondrolio@gmail.com>
2017-10-02 16:07:41 +0100

    Use bot-card instead of just card

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         discordbots.org MedalBot Remover
 // @namespace    https://moustacheminer.com/
-// @version      0.1
+// @version      0.2
 // @description  Removes the MedalBot from the Certified bots list.
 // @author       Moustacheminer Server Services
 // @match        https://discordbots.org/
 // @grant        none
 // ==/UserScript==
 
-[... document.getElementsByClassName('card')].filter(thing => thing.innerHTML.includes('MedalBot')).forEach(thing => thing.parentNode.removeChild(thing));
+[... document.getElementsByClassName('bot-card')]
+    .filter(thing => thing.innerHTML.includes('/bot/307998818547531777'))
+    .forEach(thing => thing.parentNode.removeChild(thing));
