57e5815609152c56caada1ba7f8ddead8d70d907
lepon01 <leondrolio@gmail.com>
2017-10-28 12:56:48 +0100

    Use new "partnered" class to classify bots

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // ==UserScript==
 // @name         discordbots.org AdBlock
 // @namespace    https://moustacheminer.com/
-// @version      0.3
+// @version      0.4
 // @description  Removes the Partnered Bots from the Certified bots list.
 // @author       Moustacheminer Server Services
 // @match        https://discordbots.org/
 // @grant        none
 // ==/UserScript==
 
-const partners = [
-	'307998818547531777',
-	'216816090712506378'
-];
-
-[... document.getElementsByClassName('bot-card')]
-    .filter(bot => partners.some((id) => bot.innerHTML.includes(id)))
-    .forEach(bot => bot.parentNode.removeChild(bot));
+window.addEventListener('load', () => {
+	[... document.getElementsByClassName('partnered')]
+		.forEach(bot => bot.parentNode.removeChild(bot));
+});
