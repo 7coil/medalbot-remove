e4f167085b08dfc9ca83de7f5a8433458f52ef41
lepon01 <leondrolio@gmail.com>
2017-10-28 12:45:02 +0100

    Use an array of partners instead of hardcoding

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // ==UserScript==
-// @name         discordbots.org MedalBot Remover
+// @name         discordbots.org AdBlock
 // @namespace    https://moustacheminer.com/
-// @version      0.2
-// @description  Removes the MedalBot from the Certified bots list.
+// @version      0.3
+// @description  Removes the Partnered Bots from the Certified bots list.
 // @author       Moustacheminer Server Services
 // @match        https://discordbots.org/
 // @grant        none
 // ==/UserScript==
 
+const partners = [
+	'307998818547531777',
+	'216816090712506378'
+];
+
 [... document.getElementsByClassName('bot-card')]
-    .filter(thing => thing.innerHTML.includes('/bot/307998818547531777'))
-    .forEach(thing => thing.parentNode.removeChild(thing));
+    .filter(bot => partners.some((id) => bot.innerHTML.includes(id)))
+    .forEach(bot => bot.parentNode.removeChild(bot));
